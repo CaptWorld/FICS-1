2605c4625e066e32f50c522f12b8959c456e0058
lokeshvardhan <vardhanlokesh3@gmail.com>
2023-01-28 16:10:57 +0530

    Solutions for ex-6,7

---
+++ Ch3/ex6.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex6) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; Subsitution rule for if form
+; (if true exp1 exp2) => exp1
+; (if false exp1 exp2) => exp2
